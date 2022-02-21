e99f19d8c3bdc7f335f90d900e253e13bb95f29f
jafarjtown <jafaridris82@gmail.com>
2022-02-22 00:11:11 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 (function(){
-    window.addEventListener('scroll', (e)=> {
-       let scroll = window.scrollY
-       if(scroll > 50) document.querySelector('header').classList.add('scroll-header')
-       else document.querySelector('header').classList.remove('scroll-header')
-    })
     document.getElementById('name').innerText = '<Jafar Idris/>'
+    document.getElementById('c').innerText =   `<== Design and created by Jafar Idris ==> ${new Date().getFullYear()}`
 })()
