383e561bb9523759854ea4c8847cacf6d05aae63
jafarjtown <jafaridris82@gmail.com>
2022-02-22 00:29:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function(){
     document.getElementById('name').innerText = '<Jafar Idris/>'
+    document.getElementById('submit').addEventListener('click', () => alert('Thanks for checking out!'))
     document.getElementById('c').innerText =   `<== Design and created by Jafar Idris ==> ${new Date().getFullYear()}`
 })()
